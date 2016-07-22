e4f589bfe1b1baedc46457e05408734591943312
Mauricio Vaquez B <mauricio.vasquezbernal@studenti.polito.it>
2016-07-22 10:36:32 +0200

    fix name resolver conf file

--- scripts/0_start_ovs.sh
+++ scripts/0_start_ovs.sh
@@ -6,6 +6,10 @@
 ovsdb-tool create /usr/local/etc/openvswitch/conf.db /usr/local/share/openvswitch/vswitch.ovsschema
 ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --remote=ptcp:6632 --detach > /dev/null &
 
+# mount huge pages
+mkdir /dev/hugepages
+mount -t hugetlbfs nodev /dev/hugepages
+
 # start ovs
 export DB_SOCK=/usr/local/var/run/openvswitch/db.sock
 ovs-vswitchd --dpdk -c 0x0F -n 4 -m 2048 -- unix:$DB_SOCK --pidfile
